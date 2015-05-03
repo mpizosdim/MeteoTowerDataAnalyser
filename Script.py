069417636fbadff0df9dfd8dad3931eae9ac150c
Dimitris <dimitrisbizopoulos@gmail.com>
2015-05-03 20:28:20 +0200

    ....

--- Script.py
+++ Script.py
@@ -1,14 +1,17 @@
 #Trial script
 # Packages
 import simplekml
-
+import csv
 ##
 latitude = int(input('Input the latitude of the Meteorological Tower:'))
 longitude = int(input('Input the longitude of the Meteorological Tower:'))
 NameOfTheAreaTower = input('Name the Area/Meteo Tower:')
+PathOftheFile = input('Enter the path of the file:')
 kml = simplekml.Kml()
-kml.newpoint(name=NameOfTheAreaTower,coords=[(latitude, longitude)])
+kml.newpoint(name=NameOfTheAreaTower, coords=[(latitude, longitude)])
 kml.save('NameOfTheAreaTower'+'.kml')
-print(kml)
-f = open('winddata.txt','r')
-print(f)
+
+
+
+
+
