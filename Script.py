a5c471ad5933fb1254e93dc17b73747c17aa427f
Dimitris <dimitrisbizopoulos@gmail.com>
2015-05-04 18:11:53 +0200

    panda use

--- Script.py
+++ Script.py
@@ -3,17 +3,22 @@
 import simplekml
 import numpy as np
 import pandas as pd
-from urllib.request import urlopen
 
 ##
-latitude = int(input('Input the latitude of the Meteorological Tower:'))
-longitude = int(input('Input the longitude of the Meteorological Tower:'))
-NameOfTheAreaTower = input('Name the Area/Meteo Tower:')
+##latitude = int(input('Input the latitude of the Meteorological Tower:'))
+##longitude = int(input('Input the longitude of the Meteorological Tower:'))
+##NameOfTheAreaTower = input('Name the Area/Meteo Tower:')
 PathOftheFile = input('Enter the path of the file:')
-kml = simplekml.Kml()
-kml.newpoint(name=NameOfTheAreaTower, coords=[(latitude, longitude)])
-kml.save('NameOfTheAreaTower'+'.kml')
+##kml = simplekml.Kml()
+##kml.newpoint(name=NameOfTheAreaTower, coords=[(latitude, longitude)])
+##kml.save('NameOfTheAreaTower'+'.kml')
 
+TypeOfInput = input('Are the name/type of the columns defined in the file?')
 
-df = pd.read_csv(PathOftheFile, sep='\t')
-print(df['date'])
+if TypeOfInput=='yes':
+    df = pd.read_csv(PathOftheFile, sep='\t')
+else:
+    UserInput = input('Enter the names of the types separated by commas')
+    ColumnTypes = UserInput.split(',')
+    df = pd.read_csv(PathOftheFile, sep='\t', names=ColumnTypes)
+print(df)
