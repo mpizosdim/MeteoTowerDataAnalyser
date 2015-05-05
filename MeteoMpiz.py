1d04e62b3e17b87b20f5397537545a42db59bd65
Dimitris <dimitrisbizopoulos@gmail.com>
2015-05-06 00:04:55 +0200

    panda use

--- MeteoMpiz.py
+++ MeteoMpiz.py
@@ -1,10 +1,11 @@
 import numpy as np
-
+import simplekml
 
 class Environment:
     Pressure = 'atmospheric pressure'
     AirGasConstant = 'Gas constant for air'
     AirDensity = 'Air Density'
+
     def __init__(self, Pressure=[],AirGasConstant=[], AirDensity=[],Temperature=[]):
         KelvinConstant = 273.15
         if (not Pressure) and (AirGasConstant) and (AirDensity) and (Temperature.size):
@@ -27,3 +28,24 @@ class Environment:
         print('Mean value of Air Density is {0:.2f}'.format(self.AirDensity.mean()))
         print('Gas constant of Air is %d'%self.AirGasConstant)
         print('Pressure is %d'%self.Pressure)
+
+
+class MeteoMast:
+    Height = 'Height of the Meteorological Mast'
+    VelocitySensor_Height = 'Vector with the heights of the Sensors for the Velocity'
+    DirectionSensor_Height = 'Vector with the heights of the Sensors for the Direction'
+    TemperatureSensor_Height = 'Vector with the heights of the Sensors for the Temperature'
+
+    def __init__(self, Height, VelocitySensor_Height, DirectionSensor_Height, TemperatureSensor_Height, name='Meteorological Mast', latitude=[], longitude=[]):
+        self.Height = Height
+        self.VelocitySensor_Height = VelocitySensor_Height
+        self.DirectionSensor_Height = DirectionSensor_Height
+        self.TemperatureSensor_Height = TemperatureSensor_Height
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
+        if (latitude) and (longitude):
+            kml = simplekml.Kml()
+            kml.newpoint(name=name, coords=[(latitude, longitude)])
+            kml.save(name+'.kml')
+
